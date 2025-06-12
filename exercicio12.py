048ec77d23b73023cc8fbc8080f01276ac55e0fd
leonardo-kulkamp <kulkamp.leonardo@escola.pr.gov.br>
2025-06-12 14:51:10 +0000

    exercicio12 finalizado

--- exercicio12.py
+++ exercicio12.py
@@ -1,3 +1,12 @@
 #Exercício 12 – Validação de Senha
 #Peça ao usuário uma senha e verifique se ela contém pelo menos 8 caracteres.
 #Exiba uma mensagem de "Senha válida" ou "Senha muito curta".
+
+senha = input("Digite a sua senha ")
+
+digitos = len(senha)
+
+if digitos >= 8:
+    print("Senha correta")
+else:
+    print("Senha incorreta")
