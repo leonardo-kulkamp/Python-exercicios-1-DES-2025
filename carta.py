c612301e44aab99c1b8e3197e77138caa80f9ade
leonardo-kulkamp <kulkamp.leonardo@escola.pr.gov.br>
2025-06-17 12:16:41 +0000

    carta finalizada

---
+++ carta.py
@@ -0,0 +1,9 @@
+email_destinatario = input("Digite o email do destinatario: " )
+mensagem = input("Digite sua mensagem " )
+email_remetente = input("Digite o seu email " )
+data = input("Digite a data de hoje")
+
+print("\--- Seu email ---")
+print(f"Olá {email_destinatario},\n\n{mensagem}\n\nAtenciosamente,email enviado por {email_remetente},\n\n{data}")
+
+
