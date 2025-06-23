c836efb04727d4e7f634e06751e7ec9e41c5bb93
leonardo-kulkamp <kulkamp.leonardo@escola.pr.gov.br>
2025-06-23 13:47:14 +0000

    exercicio 16 finalizado

--- exercicio16.py
+++ exercicio16.py
@@ -4,3 +4,14 @@
 #Salários até R$ 2000,00: +15%
 #De R$ 2000,01 a R$ 5000,00: +10%
 #Acima de R$ 5000,00: +5%
+
+salario = int(input("Digite o seu salario "))
+
+if salario <= 2000:
+    print("O seu reajuste de salario será de 15%")
+elif salario >= 5000:
+    print("O seu reajuste de salario será de 5%")
+else:
+    print("O seu reajuste de salario será de 10%")
+
+#Finalizado
