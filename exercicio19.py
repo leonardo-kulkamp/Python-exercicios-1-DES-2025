83d02db51f5390564bf68224dfb535a36e94535e
leonardo-kulkamp <kulkamp.leonardo@escola.pr.gov.br>
2025-06-23 13:41:28 +0000

    exercicio19 finalizado

--- exercicio19.py
+++ exercicio19.py
@@ -1,3 +1,13 @@
 #Exercício 19 – Números em Ordem Crescente
 #Peça três números e exiba-os em ordem crescente.
 
+numero01 = float(input("Digite um numero "))
+numero02 = float(input("Digite um numero "))
+numero03 = float(input("Digite um numero "))
+
+numeros = [numero01, numero02, numero03]
+
+numeros.sort()
+print("Em ordem crescente", numeros)
+
+#Finalizado
