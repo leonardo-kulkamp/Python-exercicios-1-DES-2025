3e0794e2bf746609275855ede5f26829c5eb9faf
leonardo-kulkamp <kulkamp.leonardo@escola.pr.gov.br>
2025-06-23 14:04:18 +0000

    exercicio 13 finalizado

--- exercicio13.py
+++ exercicio13.py
@@ -1,3 +1,12 @@
 #Exercício 13 – Verificação de Ano Bissexto
 #Crie um programa que receba um ano e diga se ele é bissexto.
 #(Dica: múltiplos de 4, exceto os múltiplos de 100, a menos que também sejam múltiplos de 400)
+
+ano = int(input("Digite o ano "))
+
+if ano % 100 != 0 and ano % 4 == 0 or ano % 400 == 0:
+    print("È bissexto")
+else:
+    print("Nao e bissexto")
+
+#Finalizado
