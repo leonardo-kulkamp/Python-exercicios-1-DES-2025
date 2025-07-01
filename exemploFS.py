31647ed014d6691bc680f00ec53c7c5dde23032b
leonardo-kulkamp <kulkamp.leonardo@escola.pr.gov.br>
2025-07-01 12:07:05 +0000

    procedimento simples finalizado

---
+++ exemploFS.py
@@ -0,0 +1,16 @@
+
+def bem_vindo():
+    print("Seja bem vindo ao sistema do Leonardo")
+#bem_vindo()
+
+
+#Função
+
+def soma(n1,n2):
+    return n1 + n2
+
+v1 = int(input("Digite o primeiro valor "))
+v2 = int(input("Digite o segundo valor "))
+print("A soma dos valores é: " , soma(v1,v2))
+
+bem_vindo()
---
+++ procedimentosimples.py
@@ -0,0 +1,5 @@
+
+def bem_vindo():
+    print("Seja bem vindo ao sistema do Leonardo")
+
+bem_vindo()
